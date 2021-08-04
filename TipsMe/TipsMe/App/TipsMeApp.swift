7e9384c5f7b581b86799a9ca1f61f718b181d16a
Solovyov <max-solovyov@rambler.ru>
2021-08-04 17:46:52 +0300

    v 0.1

---
+++ TipsMe/TipsMe/App/TipsMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipsMeApp.swift
+//  TipsMe
+//
+//  Created by Максим Соловьёв on 04.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct TipsMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
