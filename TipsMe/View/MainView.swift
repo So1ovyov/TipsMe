74b5a15ecba0aed406b78a9ca1a0bb528f4092b5
Solovyov <max-solovyov@rambler.ru>
2021-08-04 18:36:22 +0300

    v 0.3 App complete

--- TipsMe/View/MainView.swift
+++ TipsMe/View/MainView.swift
@@ -8,8 +8,64 @@
 import SwiftUI
 
 struct MainView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 0
+    @State private var typeCurrency = "RUB"
+    
+    let tipPercentages = [5, 10, 15, 20]
+    let typesCurrency = ["RUB", "USA", "EURO"]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                
+                Section(header: Text("Общая сумма по чеку")) {
+                    TextField("Сколько сколько ???", text: $checkAmount)
+                }
+                
+                Section(header: Text("Общее количество людей")) {
+                    Picker("Количество людей", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                
+                Section(header: Text("Процент чаевых")) {
+                    Picker("", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Валюта")) {
+                    Picker("", selection: $typeCurrency) {
+                        ForEach(typesCurrency, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Итого с каждого скидываемся по ...")) {
+                    Text("\(totalPerPerson, specifier: "%.2f") \(typeCurrency)")
+                }
+                
+            }
+            .navigationBarTitle("TipsMe", displayMode: .large)
+        }
     }
 }
 
